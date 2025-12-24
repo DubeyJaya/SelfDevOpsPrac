472c7bc287ddef2ad5d4067cbc51a34610e70a40
Khushi Dubey <jaya.2540@xcelevate.org>
2025-12-24 13:37:18 +0530

    add integration file and then rename file

---
+++ src/integration-test/java/com/practice/AppIT.java
@@ -0,0 +1,23 @@
+package com.practice;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class AppIntegrationTest {
+
+    @Test
+    public void testAddAndMultTogether() {
+        App app = new App();
+        int sum = app.add(10, 5);
+        int product = app.mult(sum, 2);
+        assertEquals(30, product);  // (10+5)*2 = 30
+    }
+
+    @Test
+    public void testSubAndDivTogether() {
+        App app = new App();
+        int diff = app.sub(20, 4);
+        int div = app.div(diff, 4);
+        assertEquals(4, div);  // (20-4)/4 = 4
+    }
+}
